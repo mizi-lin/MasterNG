92900d3de55b22a44f3bc0cfbb2a50791affde7b
amily4555 <amily4555@gmail.com>
2017-08-09 16:59:06 +0800

    changes ng-cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import * as $ from 'jquery';
+declare let mu: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+    title = 'app';
+
+    constructor() {
+        this.title = 'AAPPP';
+        console.log($);
+        console.log(mu);
+        console.log(jQuery);
+
+    }
+
+    ngAfterViewInit(): void {
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {AppComponent} from './app.component';
+import {FlexLayoutModule} from '../lib/flex-layout/flex-layout.module';
+import {PanelModule} from '../lib/panel/panel.module';
+import {ButtonModule} from '../lib/button/button.module';
+import {CalendarModule} from '../lib/calendar/calendar.module';
+import {DynamicModule} from '../lib/dynamic/dynamic.module';
+import {ReqModule} from '../lib/req/req.module';
+import {DemoPanelComponent} from './demo-panel/demo-panel.component';
+import {DemoReqComponent} from './demo-req/demo-req.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DemoPanelComponent,
+        DemoReqComponent
+    ],
+    imports: [
+        BrowserModule,
+
+        RouterModule.forRoot([
+            {
+                path: 'panel',
+                component: DemoPanelComponent
+            },
+            {
+                path: 'req',
+                component: DemoReqComponent
+            }
+        ], {useHash: true}),
+
+        FlexLayoutModule.forRoot(),
+        PanelModule.forRoot(),
+        ButtonModule.forRoot(),
+        CalendarModule.forRoot(),
+
+        DynamicModule.forRoot([
+            FlexLayoutModule,
+            PanelModule,
+            ButtonModule,
+            CalendarModule
+        ]),
+
+        ReqModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
