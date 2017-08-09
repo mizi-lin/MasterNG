8e62e1f7b8528cb5080eb295192d290d9ad79cb5
mizi.lin <mizi.lin@admaster.com.cn>
2017-08-09 16:59:06 +0800

    changes ng-cli

---
+++ src/app/demo-req/demo-req.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-req',
+  templateUrl: './demo-req.component.html',
+  styleUrls: ['./demo-req.component.scss']
+})
+export class DemoReqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
