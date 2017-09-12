df660740f12eec29929f1a692484ede6b64e72f3
mizi.lin <mizi.lin@admaster.com.cn>
2017-09-12 18:51:51 +0800

    echarts of sort

--- src/lib/mn-echarts/mn-echarts.module.ts
+++ src/lib/mn-echarts/mn-echarts.module.ts
@@ -9,6 +9,8 @@ import {MnEchartsComponent} from './mn-echarts.component';
 import {MnEchartsPanelComponent} from './mn-echarts-panel.component';
 import {EchartsService} from './echarts.service';
 import {ModuleWithProviders, NgModule} from '@angular/core';
+import {NgZorroAntdModule} from 'ng-zorro-antd/src/release/ng-zorro-antd.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
     imports: [
@@ -17,7 +19,10 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
         MnPanelModule.forRoot(),
         MnReqModule.forRoot(),
         MnLayoutModule.forRoot(),
-        MnHandsontableModule.forRoot()
+        MnHandsontableModule.forRoot(),
+
+        NgZorroAntdModule,
+        BrowserAnimationsModule
     ],
     declarations: [
         MnEchartsRenderDirective,
