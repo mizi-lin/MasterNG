dd49daccae807d266bd583d94ad5d191d48f03d2
mizi.lin <mizi.lin@admaster.com.cn>
2017-09-27 16:26:58 +0800

    echart listener resize

--- src/app/demo-echart2/demo-echart2.component.ts
+++ src/app/demo-echart2/demo-echart2.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'mn-demo-echart2',
-  templateUrl: './demo-echart2.component.html',
-  styleUrls: ['./demo-echart2.component.scss']
+    selector: 'mn-demo-echart2',
+    templateUrl: './demo-echart2.component.html',
+    styleUrls: ['./demo-echart2.component.scss']
 })
 export class DemoEchart2Component implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    height: number = 350;
+
+    ngOnInit() {
+
+        // setInterval(() => {
+        //     this.height += 20;
+        // }, 1000);
+    }
 
 }
