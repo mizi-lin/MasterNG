4a9bd874070e6d9e43d49b4e7498894a7d1ecd2a
amily4555 <amily4555@gmail.com>
2017-12-01 18:20:14 +0800

    fixed or changes

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-echo $npm_config_argv
-
-_commit=`node -pe 'JSON.parse(process.env.npm_config_argv).original[3] || "fixed changes"'`
+_commit=`node -pe 'JSON.parse(process.env.npm_config_argv).original[3] || "fixed or changes"'`
 
 git add .
 
 git commit -am "$_commit"
 
 if [ $? -eq 1 ]; then
-    echo 'has been commit && exit'
+    echo 'committed && exit'
 fi
 
+git pull
 
-
-
+git push
