d8ce7f1f588b8cbe66a7afc55a33f14ae36f60b3
amily4555 <amily4555@gmail.com>
2017-12-01 17:39:50 +0800

    0.2.14 -> v0.2.15

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ _ov=`npm view masterng version`
 
 echo '::: 升级版本号'
     cd ./publish
-    npm version patch
+    _uv=`npm version patch`
     cd ..
 
 echo ':::::: 推送到NPM'
@@ -15,8 +15,6 @@ if [ $? -eq 0 ]; then
         cp -R ./publish/package.json ./package.json
 
     echo ':::::::::::: 将编译结果提交到Git'
-        _uv=`npm view masterng version`
-
     git add .
     git commit -am "$_ov -> $_uv"
     git pull
